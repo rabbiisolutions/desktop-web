20eb19e564e51b7c22c5b24e1160f56fb6c3696b
pcf26536 <muffwaindan@gmail.com>
2019-09-14 15:12:38 +0300

    [ft #168494044] add landing section

---
+++ src/components/landing.js
@@ -0,0 +1,19 @@
+import React from "react";
+import BackGround from "../helpers/backGround";
+import Icon from "./utils/icon";
+import bulb from "../icons/idea.svg"
+import texts from "../constants/texts";
+
+function LandingSection() {
+  return (
+      <section className={"landing " + BackGround}>
+        <div className="slogan">
+          <Icon src={bulb} className="" height={6}/>
+          <span className="text">{texts.slogan}&nbsp;</span>
+        </div>
+      </section>
+  );
+}
+
+// Exporting the component
+export default LandingSection;
