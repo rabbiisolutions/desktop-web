60d13ef0e5b43b85590f8d72b90e45507f93b5e7
pcf26536 <muffwaindan@gmail.com>
2019-09-15 19:47:49 +0300

    [finish #168494086] add featured tutors section styling

--- src/components/FeaturedTutors.js
+++ src/components/FeaturedTutors.js
@@ -1,15 +1,14 @@
 import FeaturedTutor from "./sub-items/FeaturedTutor";
 import featuredTutors from "../constants/featuredTutors";
-import BackGround from "../helpers/backGround";
 import texts from "../constants/texts";
 import React from "react";
 
 
 const FeaturedTutors = () => {
   return (
-      <section className={"featured-" + BackGround()}>
+      <section className="featured">
         <div className="title">{texts.featuredTutors}</div>
-        <div className="featured">
+        <div className="featured-tutors">
           {FeaturedTutor(featuredTutors[0])}
           {FeaturedTutor(featuredTutors[1])}
         </div>
--- src/components/sub-items/FeaturedTutor.js
+++ src/components/sub-items/FeaturedTutor.js
@@ -4,7 +4,7 @@ import renderStars from "../utils/stars";
 
 const FeaturedTutor = (props) => {
   return (
-      <div className="featured" key={props.key}>
+      <div className="featured-tutor" key={props.key}>
         <div className="top">
           <span className="featured-tutor">{props.name}</span>
           <span className="tutor-description">({props.description})</span>
@@ -12,7 +12,7 @@ const FeaturedTutor = (props) => {
         <div className="comment">{props.comment}</div>
         <div className="bottom">
           <span className="areas">{props.areas}</span>
-          <span className="stars">{renderStars({shade: props.stars.shade, height: 1, key: props.key})}</span>
+          <span className="stars">{renderStars({shade: props.stars.shade, height: 2, key: props.key})}</span>
         </div>
       </div>
   );
