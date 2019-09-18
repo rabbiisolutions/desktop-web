bb2463ea93a926b9bdca97d1b71f2a9c1978c332
pcf26536 <muffwaindan@gmail.com>
2019-09-18 10:04:08 +0300

    [ch #168498884] add more to teecha steps

--- src/components/sub-items/TeeChaStep.js
+++ src/components/sub-items/TeeChaStep.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Icon from "../utils/icon";
+import moreHandler from "../../events/moreHandler";
 
 
 const TeeChaStep = (props) =>  {
@@ -8,7 +9,11 @@ const TeeChaStep = (props) =>  {
         <div className="step">Step {props.stepNumber}</div>
         <div className="step-title">&middot; {props.title}</div>
         <div className="step-icon"><Icon src={props.src} height={15} width={15}/></div>
-        <div className="step-description">{props.description}</div>
+        <div className="step-description">
+          <span>{props.description[0]}</span>
+          <span className="more hidden">{props.description[1]}</span>
+          <span className="dots" onClick={e => moreHandler(e)}>&nbsp;more...</span>
+        </div>
       </div>
   );
 };
