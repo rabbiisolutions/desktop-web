12d41db6bc34b535c79b0a5a63bc05725b9439b0
pcf26536 <muffwaindan@gmail.com>
2019-10-18 14:28:24 +0300

    [ch #168498884] update how teecha works steps

--- src/components/sub-items/TeeChaStep.js
+++ src/components/sub-items/TeeChaStep.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Icon from "../utils/icon";
-import moreHandler from "../../events/moreHandler";
 
 
 const TeeChaStep = (props) =>  {
@@ -10,9 +9,7 @@ const TeeChaStep = (props) =>  {
         <div className="step-title">&middot; {props.title}</div>
         <div className="step-icon"><Icon src={props.src} height={15} width={15}/></div>
         <div className="step-description">
-          <span>{props.description[0]}</span>
-          <span className="more hidden">{props.description[1]}</span>
-          <span className="dots" onClick={e => moreHandler(e)}>&nbsp;more...</span>
+          <span>{props.description}</span>
         </div>
       </div>
   );
