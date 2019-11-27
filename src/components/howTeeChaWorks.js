719613b402e00ad19eaffe0e4b00920b1f92447d
pcf26536 <muffwaindan@gmail.com>
2019-11-27 17:42:38 +0300

    [ft #169992210] add how teecha works section content and style

--- src/components/howTeeChaWorks.js
+++ src/components/howTeeChaWorks.js
@@ -6,7 +6,7 @@ import proInfo from "../icons/teeChaSteps/resume.svg"
 import studInfo from "../icons/teeChaSteps/resume (1).svg"
 import screen from "../icons/howTo/time.svg"
 import gets from "../icons/teeChaSteps/deal.svg"
-import verification from "../icons/teeChaSteps/icon.svg"
+import verification from "../icons/teeChaSteps/verified-account.svg"
 import placement from "../icons/howTo/recruitment.svg"
 import start from "../icons/teeChaSteps/start-line.svg"
 import evaluation from "../icons/teeChaSteps/evaluation.svg"
@@ -14,19 +14,44 @@ import counselling from "../icons/teeChaSteps/mentor.svg"
 import right from "../icons/teeChaSteps/fa-solid_long-arrow-alt-downarrow-right.svg"
 import left from "../icons/teeChaSteps/fa-solid_long-arrow-alt-downarrow-left.svg"
 import dotted from "../icons/teeChaSteps/Arrow 22dotted-down.svg"
-import down from "../icons/teeChaSteps/fa-solid_long-arrow-alt-downdown-solid.svg"
+import Icon from "./utils/icon";
+import DownArrow from "./utils/DownArrow";
+
 
 const HowTeeChaWorks = () => {
   return (
       <section className="how-teecha">
         <h2 className="how-teecha-title">{texts.howTeeChaWorks}</h2>
         <div className="how-teecha-steps">
-          <section id="tutor-steps">
-            <div className={'user-title'}>Tutor</div>
-
+          <section className={'tutor-col col'}>
+            <div className={'user-title tutor-title'}>Tutor</div>
+            <CircleWidget src={signup} text={'Tutor Sign Up'}/>
+            <DownArrow/>
+            <CircleWidget src={proInfo} text={'Give Professional Info'}/>
+            <DownArrow/>
+            <CircleWidget src={screen} text={'Screen Tutor'}/>
+            <DownArrow/>
+            <CircleWidget src={verification} text={'Tutor Verification'}/>
+            <DownArrow/>
+            <CircleWidget src={placement} text={'Tutor Placement'}/>
+            <DownArrow/>
+            <CircleWidget src={evaluation} text={'Tutor Evaluation & Management'}/>
           </section>
-          <section id="client-steps">
-            <div className={'user-title'}>Client</div>
+          <section className={'mid-col col'}>
+            <Icon src={left} width={17.26} height={1.125} className={'left-arrow'}/>
+            <Icon src={right} width={17.26} height={1.125} className={'right-arrow'}/>
+          </section>
+          <section className={'client-col col'}>
+            <div className={'user-title client-title'}>Client</div>
+            <CircleWidget src={signup} text={'Client Sign Up'}/>
+            <DownArrow/>
+            <CircleWidget src={studInfo} text={'Give Student Client Info'}/>
+            <DownArrow/>
+            <CircleWidget src={gets} text={'Gets Tutor'}/>
+            <Icon src={dotted} width={1.25} height={14.85} className={'dotted-arrow'}/>
+            <CircleWidget src={start} text={'Session Begins'}/>
+            <DownArrow/>
+            <CircleWidget src={counselling} text={'Mentoring & Counselling'}/>
           </section>
         </div>
       </section>
--- src/components/sub-items/CircleWidget.js
+++ src/components/sub-items/CircleWidget.js
@@ -5,7 +5,7 @@ const CircleWidget = (props) => {
   return (
       <div className={'circle-widget'}>
         <div className={'circle-icon'}>
-          <Icon src={props.src} className={'rounded'} width={props.width} height={props.height}/>
+          <Icon src={props.src} className={'rounded'} width={6.25} height={6.25}/>
         </div>
         <div className={'text'}>{props.text}</div>
       </div>
