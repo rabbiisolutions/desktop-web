d0a4977ee6e9b9f02896730caec0c1085a22bc47
pcf26536 <muffwaindan@gmail.com>
2019-11-27 11:15:17 +0300

    [ft #169992210] add icons

--- src/components/howTeeChaWorks.js
+++ src/components/howTeeChaWorks.js
@@ -1,21 +1,33 @@
 import React from "react";
-import TeeChaStep from "./sub-items/TeeChaStep";
-import TeeChaSteps from "../constants/TeeChaSteps";
 import texts from "../constants/texts";
-
+import CircleWidget from "./sub-items/CircleWidget";
+import signup from "../icons/howTo/application.svg"
+import proInfo from "../icons/teeChaSteps/resume.svg"
+import studInfo from "../icons/teeChaSteps/resume (1).svg"
+import screen from "../icons/howTo/time.svg"
+import gets from "../icons/teeChaSteps/deal.svg"
+import verification from "../icons/teeChaSteps/icon.svg"
+import placement from "../icons/howTo/recruitment.svg"
+import start from "../icons/teeChaSteps/start-line.svg"
+import evaluation from "../icons/teeChaSteps/evaluation.svg"
+import counselling from "../icons/teeChaSteps/mentor.svg"
+import right from "../icons/teeChaSteps/fa-solid_long-arrow-alt-downarrow-right.svg"
+import left from "../icons/teeChaSteps/fa-solid_long-arrow-alt-downarrow-left.svg"
+import dotted from "../icons/teeChaSteps/Arrow 22dotted-down.svg"
+import down from "../icons/teeChaSteps/fa-solid_long-arrow-alt-downdown-solid.svg"
 
 const HowTeeChaWorks = () => {
   return (
       <section className="how-teecha">
         <h2 className="how-teecha-title">{texts.howTeeChaWorks}</h2>
         <div className="how-teecha-steps">
-          { TeeChaStep(TeeChaSteps[0]) }
+          <section id="tutor-steps">
+            <div className={'user-title'}>Tutor</div>
 
-          { TeeChaStep(TeeChaSteps[1]) }
-
-          { TeeChaStep(TeeChaSteps[2]) }
-
-          { TeeChaStep(TeeChaSteps[3]) }
+          </section>
+          <section id="client-steps">
+            <div className={'user-title'}>Client</div>
+          </section>
         </div>
       </section>
   );
