55923337b4bb9c5fd5d3eebad021fcea24907570
pcf26536 <muffwaindan@gmail.com>
2019-12-13 17:09:04 +0300

    [ft #169992210] add teecha section styling

--- src/components/SignUpModal.jsx
+++ src/components/SignUpModal.jsx
@@ -6,14 +6,14 @@ const SignUpModal = (props) => {
   return (
       <div className={props.className}>
         <div className={"close-icon " + props.status} onClick={e => signUpHandler(e)}>&#10006;</div>
-        <div className="sign-up-text">{props.signUpText}</div>
+        <div id="sign-up-text">{props.signUpText}</div>
         <section>
           <div className="user-title">
             Sign up as a Client/Parent to get a tutor/Teacher
           </div>
           <div id="client-btns">
-            <ButtonLink link="https://forms.gle/y7KTkMDtYCtAywDu6" className="clients-btn" value="Get a Tutor"/>
-            <ButtonLink link="https://forms.gle/3zg3FSExEBdBPHd39" className="clients-btn" value="Referral"/>
+            <ButtonLink link="https://forms.gle/y7KTkMDtYCtAywDu6" className="client-btn" value="Get a Tutor"/>
+            <ButtonLink link="https://forms.gle/3zg3FSExEBdBPHd39" className="client-btn" value="Referrals"/>
           </div>
         </section>
         <section>
@@ -21,12 +21,12 @@ const SignUpModal = (props) => {
             Register to be a Tutor/Teacher in your specific category
           </div>
           <div id="tutor-btns">
-            <ButtonLink link="https://forms.gle/LerPUYib9EJ3yMxr7" className="tutors-btn" value="Non TSC Tutor"/>
-            <ButtonLink link="https://forms.gle/ZU8GZrYVPWenL4r19" className="tutors-btn" value="TSC Tutor"/>
-            <ButtonLink link="https://forms.gle/CmgUyZCWrnLgsqnC9" className="tutors-btn" value="Skills Tutor"/>
+            <ButtonLink link="https://forms.gle/LerPUYib9EJ3yMxr7" className="tutor-btn" value="Non TSC Tutor"/>
+            <ButtonLink link="https://forms.gle/ZU8GZrYVPWenL4r19" className="tutor-btn" value="TSC Tutor"/>
+            <ButtonLink link="https://forms.gle/CmgUyZCWrnLgsqnC9" className="tutor-btn" value="Skills Tutor"/>
           </div>
         </section>
-        <div className="msg">If you face difficulty in registering call, text or whatsapp on 0772 915 361</div>
+        <div className="info">If you face difficulty in registering call, text or WhatsApp on 0772 915 361</div>
       </div>
   );
 };
--- src/components/TeeCha.jsx
+++ src/components/TeeCha.jsx
@@ -3,48 +3,59 @@ import texts from "../constants/Texts";
 import teeChaAreasLists from "./composite/TeeChaAreasList";
 import SignUpModal from "./SignUpModal";
 import ButtonLink from "./basic/ButtonLink.jsx";
+import teechaJ from "../images/teecha.jpg";
+import teechaW from "../images/teecha.webp";
+import ImageWebp from "../helpers/ImageWebp";
 
 const TeeCha = () => {
   return (
     <section id={"teecha"}>
-        <h2 className="teecha-caption">
-          <div className="teecha">{texts.teeCha}</div>
-          <div className="home-tuition">{texts.homeTuition}</div>
-        </h2>
-        <div className="boxes">
-          <div className="teecha-box">
-          <div className="home-tuition-areas">{texts.homeTuitionAreas}</div>
-          <div className="teecha-areas">
-            <div className="col-1">
-              <div className="category">Academic Tuition</div>
-              {teeChaAreasLists.academics}
-            </div>
-            <div className="col-2">
-              <div className="category">Skills Tuition</div>
-              <div className="skills">
-                <div className="fl">
-                  <div className="sub-category">Foreign Languages</div>
-                  {teeChaAreasLists.fl}
-                </div>
-                <div className="gs">
-                  <div className="sub-category">Games & Sports</div>
-                  {teeChaAreasLists.gs}
+      <h2 id="teecha-caption">
+        <div id="teecha-title">{texts.teeCha}</div>
+        <a id="caption" href={'https://catalogue.rabbii.co.ke'}>{texts.homeTuition}</a>
+      </h2>
+      <div id={'teecha-row'}>
+        <ImageWebp
+            src={teechaJ}
+            srcWebp={teechaW}
+            className={'teecha-image'}
+            alt={'student'}
+        />
+        <div id="boxes">
+          <div id="teecha-box">
+            <div id="home-tuition-areas">{texts.homeTuitionAreas}</div>
+            <div id="teecha-areas">
+              <div className="col-1">
+                <div className="category">Academic Tuition</div>
+                {teeChaAreasLists.academics}
+              </div>
+              <div className="col-2">
+                <div className="category">Skills Tuition</div>
+                <div id="skills">
+                  <div id="fl">
+                    <div className="sub-category">Foreign Languages</div>
+                    {teeChaAreasLists.fl}
+                  </div>
+                  <div id="gs">
+                    <div className="sub-category">Games & Sports</div>
+                    {teeChaAreasLists.gs}
+                  </div>
                 </div>
               </div>
             </div>
+            <div id={'catalogue'}>
+              <ButtonLink link="https://catalogue.rabbii.co.ke" className="services" value="Learn more about Our Services"/>
+            </div>
+            <div className="info">
+              {texts.homeTuitionInfo}
+            </div>
           </div>
           <div id={'tuition-jobs'}>
-            <ButtonLink link="https://catalogue.rabbii.co.ke" className="clients-btn" value="Learn more about Our Services"/>
-          </div>
-          <div className="teecha-info">
-            {texts.homeTuitionInfo}
-          </div>
-        </div>
-          <div id={'catalogue'}>
-            <ButtonLink link="https://tuition-jobs.rabbii.co.ke" className="clients-btn" value="View Available Tuition Jobs"/>
+            <ButtonLink link="https://tuition-jobs.rabbii.co.ke" className="services" value="View Available Tuition Jobs"/>
           </div>
           <SignUpModal className="sign-up box" signUpText="Sign Up to Receive Our Price List" status="hidden"/>
         </div>
+      </div>
     </section>
   );
 };
