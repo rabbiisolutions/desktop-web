ca90b7536b20faa754a9e7fb3cdbfbaf598512c1
pcf26536 <muffwaindan@gmail.com>
2019-12-13 14:48:20 +0300

    [ft #169992210] add landing styling

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Icon from "./basic/Icon";
-import bulb from "../icons/ideabulb.svg"
+import bulb from "../icons/idea.svg"
 import texts from "../constants/Texts";
 import landingJ from "../images/landing.jpg";
 import landingW from "../images/landing.webp";
@@ -8,18 +8,18 @@ import ImageWebp from "../helpers/ImageWebp";
 
 function Landing() {
   return (
-      <section id={"landing-image"}>
+      <header  id={"landing-image"}>
         <ImageWebp
             src={landingJ}
             srcWebp={landingW}
             className={'landing-image'}
             alt={'joyous-student'}
         />
-        <div className="slogan">
-          <Icon src={bulb} height={6}/>
+        <div id="slogan">
+          <Icon src={bulb} height={8} width={8} units={'vw'} className={'idea'}/>
           <span className="text">{texts.slogan}</span>
         </div>
-      </section>
+      </header>
   );
 }
 
