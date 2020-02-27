a73a2a2433faaf96ddaf83c47fb97e00e81dee8f
pcf26536 <muffwaindan@gmail.com>
2020-02-27 15:22:39 +0300

    [ft #170866479] update menu order and lead text

--- src/constants/texts.js
+++ src/constants/texts.js
@@ -16,9 +16,9 @@ const safaricom = '0798 884 419';
 const telkom = '0772 915 361';
 const sema = 'sema@rabbii.co.ke';
 const menuTitle = 'Menu';
-const menuItems = ['Payment', 'Become a Tutor', 'About Us', 'Trending', 'FAQs'];
-const menuLinks = ['https://payment.rabbii.co.ke', 'https://tutor.rabbii.co.ke', 'https://about.rabbii.co.ke',
-  'https://trending.rabbii.co.ke', 'https://faqs.rabbii.co.ke'];
+const menuItems = ['Payment', 'Become a Tutor', 'About Us', 'FAQs', 'Trending'];
+const menuLinks = ['https://payment.rabbii.co.ke', 'https://tutor.rabbii.co.ke',
+  'https://about.rabbii.co.ke', 'https://faqs.rabbii.co.ke', 'https://trending.rabbii.co.ke'];
 const ourServices = ['TeeCha'];
 const hidden = 'hidden';
 const welcome = 'Welcome to rabbii';
