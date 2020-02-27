a510ee73c4e8d829d89f255610531238a37859bb
pcf26536 <muffwaindan@gmail.com>
2020-02-27 16:19:42 +0300

    [ft #170866479] update sign up modal

--- src/components/SignUpModal.jsx
+++ src/components/SignUpModal.jsx
@@ -1,31 +1,38 @@
 import React from "react";
 import ButtonLink from "./basic/ButtonLink.jsx";
 import signUpHandler from "../events/SignUpModal";
+import texts from "../constants/texts";
+import close from "../assets/icons/close.svg";
+import Icon from "./basic/Icon";
 
-const SignUpModal = (props) => {
+const SignUpModal = () => {
   return (
-      <div className={props.className}>
-        <div className={"close-icon " + props.status} onClick={e => signUpHandler(e)}>&#10006;</div>
-        <div id="sign-up-text">{props.signUpText}</div>
-        <section>
-          <div className="user-title">
-            clients/parents only
+      <div className={'sign-up-modal hidden'}>
+        <section className={'header'}>
+          <Icon src={close} className="close-icon" height={2.25} width={5} onClick={e => signUpHandler(e)}/>
+          <div id="sign-up-text">{texts.signUp}</div>
+        </section>
+        <section className={'body'}>
+          <div className={'content'}>
+            <div className="user-title">
+              clients/parents only
+            </div>
+            <div id="client-btns">
+              <ButtonLink link="https://forms.gle/y7KTkMDtYCtAywDu6" className="client-btn" value="Get a Tutor"/>
+            </div>
           </div>
-          <div id="client-btns">
-            <ButtonLink link="https://forms.gle/y7KTkMDtYCtAywDu6" className="client-btn" value="Get a Tutor"/>
+          <div className={'content'}>
+            <div className="user-title">
+              teachers/tutors only
+            </div>
+            <div id="tutor-btns">
+              <ButtonLink link="https://tutor.rabbii.co.ke" className="tutor-btn" value="Become a Tutor"/>
+            </div>
           </div>
         </section>
-        <section>
-          <div className="user-title">
-            teachers/tutors only
-          </div>
-          <div id="tutor-btns">
-            <ButtonLink link="https://forms.gle/LerPUYib9EJ3yMxr7" className="tutor-btn" value="Non TSC Tutor"/>
-            <ButtonLink link="https://forms.gle/ZU8GZrYVPWenL4r19" className="tutor-btn" value="TSC Tutor"/>
-            <ButtonLink link="https://forms.gle/CmgUyZCWrnLgsqnC9" className="tutor-btn" value="Skills Tutor"/>
-          </div>
+        <section className={'footer'}>
+          <div className="info">If you face difficulty in registering call, text or WhatsApp on 0772 915 361</div>
         </section>
-        <div className="info">If you face difficulty in registering call, text or WhatsApp on 0772 915 361</div>
       </div>
   );
 };
